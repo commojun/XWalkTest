a823573d7148333ce00816dcf72926720f8a5e38
junpooooow <junpooooow@gmail.com>
2016-04-08 20:12:23 +0900

    各種コメントの追加

--- app/src/main/java/com/commojun/xwalktest/MainActivity.java
+++ app/src/main/java/com/commojun/xwalktest/MainActivity.java
@@ -14,9 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Viewの設定とURLの読み込み
         xWalkWebView = (XWalkView)findViewById(R.id.xwalkWebView);
-        xWalkWebView.load("file:///android_asset/index.html", null);
+        //xWalkWebView.load("http://commojun.com", null);
 
+        //端末内のファイルを読み込みたい時には下記のとおりにする．
+        xWalkWebView.load("file:///android_asset/index.html", "");
+
+        //ネイティブ側からWeb側へデータを送りたいときはこのようにする．
+        //要するに，ネイティブ側から下記のようにJSが実行できるということ．
+        xWalkWebView.evaluateJavascript("var hoge = 'こんにちは！'", null);
 
         // turn on debugging
         XWalkPreferences.setValue(XWalkPreferences.REMOTE_DEBUGGING, true);
